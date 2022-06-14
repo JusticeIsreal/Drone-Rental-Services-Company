0977e36713f34022ecf1d928126960f51a28ab26
Justice Isreal <justiceyba@gmail.com>
2022-06-14 15:01:11 +0100

    added side bar to mobile view

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,3 +7,23 @@ menuBar.addEventListener("click", () => {
     sideBar.classList.toggle("active")
 
 });
+
+
+// CREATE TABLE
+
+// USE DATABASE student
+// CREATE TABLE person(
+// id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+//     first_name VARCHAR(50),
+//     last_name VARCHAR(50),
+//     email VARCHAR(50),
+//     password VARCHAR(50),
+//     date_of_birth DATE,
+//     place_of_birth VARCHAR(23)
+// );
+// // INSERT
+// INSERT INTO person (`first_name`, `last_name`,`email`,`password`,`date_of_birth`,`place_of_birth`)
+// VALUE("justice", "Agbonma","justiceyba@gmail.com","justyba", "2001-01-30","USA")
+
+// // TO COUNT NUMBER OF ROW
+// SELECT COUNT(*) FROM person

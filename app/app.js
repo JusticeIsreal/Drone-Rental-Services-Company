49f3591c10f6c26519dbfb4a4fa48e9fb9d07de4
Justice Isreal <justiceyba@gmail.com>
2022-06-20 03:54:56 +0100

    making prefect responsiveness

--- app/app.js
+++ app/app.js
@@ -1,9 +1,48 @@
-let bt = document.querySelector(".sidebar-left");
-let btn1 = document.querySelector(".bx-menu");
-let x = 55;
+// function to toggle side nav for tablet and mobile view
+let sideNav = document.querySelector(".sidebar-left");
+let menuBtn = document.querySelector(".bx-menu");
 
-btn1.addEventListener("click", () => {
-  console.log("boy");
-  btn1.classList.toggle("rotate");
-  bt.classList.toggle("open");
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("rotate");
+  sideNav.classList.toggle("open");
+});
+
+// function for card effect
+let box1 = document.querySelector(".box1");
+let box2 = document.querySelector(".box2");
+let box3 = document.querySelector(".box3");
+
+// card names
+let regularTxt = document.querySelector(".regular-txt");
+let standardTxt = document.querySelector(".standard-txt");
+let premiumTxt = document.querySelector(".premium-txt");
+
+// cards drone
+let regular = document.querySelector(".regular");
+let standard = document.querySelector(".standard");
+let premium = document.querySelector(".premium");
+
+box1.addEventListener("click", () => {
+  regularTxt.classList.toggle("grade-txt");
+  standardTxt.classList.remove("grade-txt");
+  premiumTxt.classList.remove("grade-txt");
+  regular.classList.toggle("drone-ani");
+  standard.classList.remove("drone-ani");
+  premium.classList.remove("drone-ani");
+});
+box2.addEventListener("click", () => {
+  standardTxt.classList.toggle("grade-txt");
+  premiumTxt.classList.remove("grade-txt");
+  regularTxt.classList.remove("grade-txt");
+  standard.classList.toggle("drone-ani");
+  regular.classList.remove("drone-ani");
+  premium.classList.remove("drone-ani");
+});
+box3.addEventListener("click", () => {
+  premiumTxt.classList.toggle("grade-txt");
+  standardTxt.classList.remove("grade-txt");
+  regularTxt.classList.remove("grade-txt");
+  premium.classList.toggle("drone-ani");
+  standard.classList.remove("drone-ani");
+  regular.classList.remove("drone-ani");
 });

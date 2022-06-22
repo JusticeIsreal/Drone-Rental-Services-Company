ff2a68e8439935882d0ec6cc4499c16af79b76a0
Justice Isreal <justiceyba@gmail.com>
2022-06-22 04:39:29 +0100

    include google chart

--- app/app.js
+++ app/app.js
@@ -61,90 +61,79 @@ formContainerRemove.addEventListener("click", () => {
 
 // jkhjhjhkjhjkhjkl
 
+google.charts.load("current", { packages: ["gauge"] });
+google.charts.setOnLoadCallback(drawChart);
 
- google.charts.load("current", { packages: ["gauge"] });
- google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Label", "Value"],
+    ["Memory", 80],
+    ["CPU", 55],
+    ["Network", 68],
+  ]);
 
- function drawChart() {
-   var data = google.visualization.arrayToDataTable([
-     ["Label", "Value"],
-     ["Memory", 80],
-     ["CPU", 55],
-     ["Network", 68],
-   ]);
+  var options = {
+    width: 400,
+    height: 120,
+    redFrom: 90,
+    redTo: 100,
+    yellowFrom: 75,
+    yellowTo: 90,
+    minorTicks: 5,
+  };
 
-   var options = {
-     width: 400,
-     height: 120,
-     redFrom: 90,
-     redTo: 100,
-     yellowFrom: 75,
-     yellowTo: 90,
-     minorTicks: 5,
-   };
+  var chart = new google.visualization.Gauge(
+    document.getElementById("chart_div")
+  );
 
-   var chart = new google.visualization.Gauge(
-     document.getElementById("chart_div")
-   );
+  chart.draw(data, options);
 
-   chart.draw(data, options);
+  setInterval(function () {
+    data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+    chart.draw(data, options);
+  }, 3000);
+  setInterval(function () {
+    data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+    chart.draw(data, options);
+  }, 5000);
+  setInterval(function () {
+    data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+    chart.draw(data, options);
+  }, 2000);
+}
 
-   setInterval(function () {
-     data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
-     chart.draw(data, options);
-   }, 3000);
-   setInterval(function () {
-     data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
-     chart.draw(data, options);
-   }, 5000);
-   setInterval(function () {
-     data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
-     chart.draw(data, options);
-   }, 2000);
- }
+//  bar chart
 
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawCha);
 
-//  bar chart 
+function drawCha() {
+  var data = google.visualization.arrayToDataTable([
+    ["Year", "Trips ordered"],
+    ["Jan", 10],
+    ["Feb", 11],
+    ["Mar", 6],
+    ["Apr", 20],
+    ["May", 1],
+    ["Jun", 10],
+    ["Jul", 0],
+    ["Aug", 0],
+    ["Sep", 0],
+    ["Oct", 0],
+    ["Nov", 0],
+    ["Dec", 0],
+  ]);
 
-  google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChar);
-      function drawChar() {
-        var data = google.visualization.arrayToDataTable([
-          ['Dinosaur', 'Length'],
-          ['Acrocanthosaurus (top-spined lizard)', 12.2],
-          ['Albertosaurus (Alberta lizard)', 9.1],
-          ['Allosaurus (other lizard)', 12.2],
-          ['Apatosaurus (deceptive lizard)', 22.9],
-          ['Archaeopteryx (ancient wing)', 0.9],
-          ['Argentinosaurus (Argentina lizard)', 36.6],
-          ['Baryonyx (heavy claws)', 9.1],
-          ['Brachiosaurus (arm lizard)', 30.5],
-          ['Ceratosaurus (horned lizard)', 6.1],
-          ['Coelophysis (hollow form)', 2.7],
-          ['Compsognathus (elegant jaw)', 0.9],
-          ['Deinonychus (terrible claw)', 2.7],
-          ['Diplodocus (double beam)', 27.1],
-          ['Dromicelomimus (emu mimic)', 3.4],
-          ['Gallimimus (fowl mimic)', 5.5],
-          ['Mamenchisaurus (Mamenchi lizard)', 21.0],
-          ['Megalosaurus (big lizard)', 7.9],
-          ['Microvenator (small hunter)', 1.2],
-          ['Ornithomimus (bird mimic)', 4.6],
-          ['Oviraptor (egg robber)', 1.5],
-          ['Plateosaurus (flat lizard)', 7.9],
-          ['Sauronithoides (narrow-clawed lizard)', 2.0],
-          ['Seismosaurus (tremor lizard)', 45.7],
-          ['Spinosaurus (spiny lizard)', 12.2],
-          ['Supersaurus (super lizard)', 30.5],
-          ['Tyrannosaurus (tyrant lizard)', 15.2],
-          ['Ultrasaurus (ultra lizard)', 30.5],
-          ['Velociraptor (swift robber)', 1.8]]);
+  var options = {
+    title: "Drone Statistics",
+    curveType: "function",
+    legend: { position: "bottom" },
+  };
 
-        var options = {
-          title: 'Lengths of dinosaurs, in meters',
-          legend: { position: 'none' },
-        };
+  var chart = new google.visualization.LineChart(
+    document.getElementById("curve_chart")
+  );
 
-        var chart = new google.visualization.Histogram(document.getElementById('chart_di'));
-        chart.draw(data, options);
-      }
+  chart.draw(data, options);
+ 
+}

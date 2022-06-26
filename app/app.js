711a56a475c2979229f714f49bba9ebeb2c93243
Justice Isreal <justiceyba@gmail.com>
2022-06-26 16:52:30 +0100

    added some javascript functionalities to control dashboard display

--- app/app.js
+++ app/app.js
@@ -1,3 +1,162 @@
+// create API for the drone details
+const detailsApi = [
+  {
+    id: 1,
+    droneName: "...",
+    class: "...",
+    serialNo: "...",
+    weightModel: "...",
+    weightLimit: "...",
+    batteryCapacity: "...",
+    flightTime: "...",
+    mP: "...",
+    POV: "...",
+    RES: "...",
+    MD: "...",
+    time: "...",
+    tmp: "...",
+    vlt: "...",
+    state: "...",
+  },
+  {
+    id: 2,
+    droneName: "Static Tropers",
+    class: "REGULAR",
+    serialNo: "025",
+    weightModel: "Light Weight",
+    weightLimit: "250g",
+    batteryCapacity: "3000 mAh",
+    flightTime: "1hr : 30min  flight time",
+    mP: "35 MP",
+    POV: "18* X 28*",
+    RES: "380 x 410",
+    MD: "Sandard",
+    time: "1hr : 30min ",
+    tmp: "7 deg-C",
+    vlt: "28.5 V",
+    state: "IDLE ...",
+  },
+  {
+    id: 3,
+    droneName: "StrinCode Air Drift",
+    class: "STANDARD",
+    serialNo: "010",
+    weightModel: "medium Weight",
+    weightLimit: "350g",
+    batteryCapacity: "5000 mAh",
+    flightTime: "2hr flight time",
+    mP: "45 MP",
+    POV: "28* X 36*",
+    RES: "400* x 510*",
+    MD: "Panasonic",
+    time: "3 hours",
+    tmp: "6 deg-C",
+    vlt: "34.5 V",
+    state: "IDLE ...",
+  },
+  {
+    id: 4,
+    droneName: "JBlaze Air drone",
+    class: "PREMIUM",
+    serialNo: "169",
+    weightModel: "Heavy Weight",
+    weightLimit: "500g",
+    batteryCapacity: "5000 mAh",
+    flightTime: "4 hrs flight time",
+    mP: "75 MP",
+    POV: "50* X 68*",
+    RES: "580* x 710*",
+    MD: "Thermal",
+    time: "4 hours",
+    tmp: "5 deg-C",
+    vlt: "45.5 V",
+    state: "IDLE ...",
+  },
+];
+
+// call respective sections container
+let droneClassCon = document.getElementById("class");
+let droneNameCon = document.querySelector(".name-con");
+let cameraSpecCon = document.querySelector(".camera-spec");
+let cameraSpecCon2 = document.querySelector(".camera-spec2");
+let droneStateCon = document.querySelector(".drone-state");
+
+// create empty string value to loop array
+
+let obj = "0";
+// function to set all values when called upon
+const displayData = (details) => {
+  let datas = details[obj];
+
+  // console.log(datas);
+  let droneClass = `<span>${datas.class} </span>`;
+  let droneName = `<div class="name-con">
+                  <h3>${datas.droneName}: <span>${datas.serialNo}</span></h3>
+                  <h4>Weight model : <span>${datas.weightModel}</span></h4>
+                  <h4>Weight limit : <span>${datas.weightLimit}</span></h4>
+                  <h4>${datas.batteryCapacity} : <span>${datas.flightTime}</span></h4>
+
+                  <h5>Flight time of 2 hrs remaining</h5>
+                </div>`;
+  let cameraSpec = `<class="camera-spec">
+                <h3><i class="bx bx-camera-movie"></i>Camera type</h3>
+                <p>MP <span>${datas.mP}</span></p>
+                <p>POV <span>${datas.POV}</span></p>
+                <p>RES <span>${datas.RES}</span></p>
+                <p>MD <span>${datas.MD}</span></p>`;
+
+  let cameraSpec2 = `          
+               <div class="camera-spec2">
+                  <h3><i class="bx bx-time"></i>Flight time</h3>
+                <p>TIME <span>${datas.time}</span></p>
+                <p>TMP <span>${datas.tmp}</span></p>
+                <p>VLT <span>${datas.vlt}</span></p>
+              </div>
+              `;
+  let droneState = `<p class="drone-state">
+                    <b><span>${datas.state}</span></b>
+                  </p>`;
+  droneClassCon.innerHTML = droneClass;
+  droneNameCon.innerHTML = droneName;
+  cameraSpecCon.innerHTML = cameraSpec;
+  cameraSpecCon2.innerHTML = cameraSpec2;
+  droneStateCon.innerHTML = droneState;
+};
+displayData(detailsApi);
+
+// call drones
+let spinDroneCon = document.querySelector(".spin-drone-con");
+let droneCon = document.querySelectorAll(".drone");
+// let droneCon = document.querySelectorAll(".drone");
+// let droneCon = document.querySelectorAll(".drone");
+let spinDrone = document.querySelector(".spin-drone");
+
+droneCon.forEach(function (drone) {
+  drone.addEventListener("click", (e) => {
+    let src = e.currentTarget.src;
+    let droneItem = e.currentTarget.dataset.id;
+    if (droneItem == "regular") {
+      obj = 1;
+      console.log(obj);
+      displayData(detailsApi);
+    }
+    if (droneItem == "standard") {
+      obj = 2;
+      console.log(obj);
+      displayData(detailsApi);
+    }
+    if (droneItem == "premium") {
+      obj = 3;
+      console.log(obj);
+      displayData(detailsApi);
+    }
+
+    let displayClass = `<img src=${src} alt="" />`;
+    spinDroneCon.innerHTML = displayClass;
+    //  console.log (spinDrone);
+  });
+});
+
 // function to toggle side nav for tablet and mobile view
 let sideNav = document.querySelector(".sidebar-left");
 let menuBtn = document.querySelector(".bx-menu");
@@ -61,7 +220,6 @@ formContainerRemove.addEventListener("click", () => {
 
 // drone gauge.
 
-
 google.charts.load("current", { packages: ["gauge"] });
 google.charts.setOnLoadCallback(drawChart);
 
@@ -103,7 +261,6 @@ function drawChart() {
 }
 
 //  bar chart
-
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawCha);
 

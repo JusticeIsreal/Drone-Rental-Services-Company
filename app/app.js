92163366c5ad4c0376c00c97bd27aee344633252
Justice Isreal <justiceyba@gmail.com>
2022-06-27 18:40:04 +0100

    adding color styling to dashboard page

--- app/app.js
+++ app/app.js
@@ -219,18 +219,18 @@ formContainerRemove.addEventListener("click", () => {
 // drone gauge.
 
 google.charts.load("current", { packages: ["gauge"] });
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart1);
 
-function drawChart() {
+function drawChart1() {
   var data = google.visualization.arrayToDataTable([
     ["Label", "Value"],
     ["CPU", 55],
-    ["Network", 68],
+    
   ]);
 
   var options = {
-    width: 400,
-    height: 120,
+    width: 230,
+    height: 80,
     redFrom: 90,
     redTo: 100,
     yellowFrom: 75,
@@ -239,7 +239,47 @@ function drawChart() {
   };
 
   var chart = new google.visualization.Gauge(
-    document.getElementById("chart_div")
+    document.getElementById("chart_div1")
+  );
+
+  chart.draw(data, options);
+
+  setInterval(function () {
+    data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+    chart.draw(data, options);
+  }, 3000);
+  setInterval(function () {
+    data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+    chart.draw(data, options);
+  }, 5000);
+  setInterval(function () {
+    data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+    chart.draw(data, options);
+  }, 2000);
+}
+
+
+google.charts.load("current", { packages: ["gauge"] });
+google.charts.setOnLoadCallback(drawChart2);
+
+function drawChart2() {
+  var data = google.visualization.arrayToDataTable([
+    ["Label", "Value"],
+      ["Network", 68],
+  ]);
+
+  var options = {
+    width: 230,
+    height: 80,
+    redFrom: 90,
+    redTo: 100,
+    yellowFrom: 75,
+    yellowTo: 90,
+    minorTicks: 5,
+  };
+
+  var chart = new google.visualization.Gauge(
+    document.getElementById("chart_div2")
   );
 
   chart.draw(data, options);
